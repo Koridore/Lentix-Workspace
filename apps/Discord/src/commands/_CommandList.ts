45d92937b88c81c625b1fd5409c3192a9aaed2ab
Koridore <jamesrichardpryor@gmail.com>
2023-01-22 06:04:40 -0500

    Initial Commit

--- apps/Discord/src/commands/_CommandList.ts
+++ apps/Discord/src/commands/_CommandList.ts
@@ -1,8 +1,4 @@
 import { CommandInt } from "../interfaces/CommandInt";
-import { edit } from "./edit";
-import { help } from "./help";
-import { oneHundred } from "./oneHundred";
-import { reset } from "./reset";
-import { view } from "./view";
 
-export const CommandList: CommandInt[] = [oneHundred, view, help, edit, reset];
+
+export const CommandList: CommandInt[] = [];
--- apps/Discord/src/index.ts
+++ apps/Discord/src/index.ts
@@ -1,50 +1,68 @@
-import { __awaiter } from "tslib";
+// Workplace: ~root~ ../apps/Discord/src/index.ts
 
-import * as Sentry from "@sentry/node";
-import { RewriteFrames } from "@sentry/integrations";
-import { validateEnv } from "./utils/validateEnv";
-// import { validator } from "envalid";
+// Imports - 
+import * as Sentry from "@sentry/node"; //
+import { RewriteFrames } from "@sentry/integrations"; //
 
+// Utils
+import { validateEnv } from "./utils/validateEnv"; // validatEnv
+
+// import { validator } from "envalid"; //
+
+// Discords API
 import {
-    Client,ClientOptions, GatewayIntentBits, Collection,PermissionFlagsBits,
-    ClientApplication,InteractionType,Interaction,CommandInteraction,
-    Partials,ButtonInteraction,ModalSubmitInteraction,SelectMenuInteraction,
-} from "discord.js";
+    Client,
+    ClientOptions,
+    GatewayIntentBits,
+    Collection,
+    PermissionFlagsBits,
+    ClientApplication,
+    Interaction,
+    InteractionType,
+    CommandInteraction,
+    ButtonInteraction,
+    ModalSubmitInteraction,
+    Partials,
+} from "discord.js"; //
 
+// Databases
 import { connectDatabase } from "./database/connectDatabase";
 
+// Events
 import { onReady } from "./events/onReady";
 import { onInteraction } from "./events/onInteraction";
 
-import { IntentOptions } from "./config/IntentOptions";
+// Configs
+import { IntentOptions } from "./config/IntentOptions"; // IntentOptions
 
+// Types
 import { Command, SlashCommand } from "../../Discord/src/types";
 
-import * as dotenv from "dotenv";
-dotenv.config()
+//
+import * as dotenv from "dotenv"; // dotenv
 
+dotenv.config() //
+
+// 
 import { readdirSync } from "fs";
 import { join } from "path";
 
+// Opening validateEnv
 (async () => {
-    validateEnv();
-  
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      tracesSampleRate: 1.0,
-      integrations: [
-        new RewriteFrames({
-          root: global.__dirname,
-        }),
-      ],
-    });
+  validateEnv();
 
-    function validateEnv() {
-        throw new Error("Function not implemented.");
-    }
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+    integrations: [
+      new RewriteFrames({
+        root: global.__dirname,
+      }),
+    ],
+  }); console.log("Success! - validated bot token..."); //
 
-const {Guilds, MessageContent, GuildMessages, GuildMembers} = GatewayIntentBits
-
+  // Client Intents
+const { Guilds, MessageContent, GuildMessages, GuildMembers } = GatewayIntentBits
 const client = new Client({
     intents:[
         Guilds, 
@@ -52,30 +70,32 @@ const client = new Client({
         GuildMessages, 
         GuildMembers
     ]
-})
+});
 
-client.on("ready", async () => await onReady(client));
+//
+client.on("ready", async () => await onReady(client)); console.log("Success! - bot is starting...");
 
-console.log("Bot is starting...");
+//
+client.on("interactionCreate", async (interaction) => await onInteraction(interaction));
 
-client.on(
-    "interactionCreate",
-    async (interaction) => await onInteraction(interaction)
-  );
-
+//
 client.slashCommands = new Collection<string, SlashCommand>()
 client.commands = new Collection<string, Command>()
 client.cooldowns = new Collection<string, number>()
 
+//Redirects
 const handlersDir = join(__dirname, "./handlers")
 readdirSync(handlersDir).forEach(handler => {
     require(`${handlersDir}/${handler}`)(client)
-})
+}); console.log(client);
 
-console.log(client);
+// Connections
+await connectDatabase(); // Awaiting database connection
 
-await connectDatabase();
+await client.login(process.env.TOKEN as string); // Awaiting bots connection
 
-await client.login(process.env.TOKEN as string)
+} // Closes validateEnv();
 
-})();
+) // Closes (async () => {
+
+(); // Prints Missing Discord bot token error
